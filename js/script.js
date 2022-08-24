e315f7b65d59db08cce4f1d4982485536ae40b0a
George <Georgesimbe@icloud.com>
2022-08-24 23:13:07 +0930

    Winning and drawing works

--- js/script.js
+++ js/script.js
@@ -1,23 +1,84 @@
 
 let board = document.querySelectorAll('.box')
-
-
 let turn = true;
-
 function game(board) {
+  for (let i = 0; i < board.length; i++) {
+    board[i].addEventListener('click', (event) => {
+      if (winner(board, turn ? 'player1' : 'player2')) {
+        i = board.length
+      } else if (board[i].getAttribute("style") == null || board[i].getAttribute("style") == "") {
+        if (turn) {
+          event.target.classList.add('player1')
+          event.target.style.backgroundColor = 'red'
+          turn = false
+        } else {
+          event.target.classList.add('player2')
+          event.target.style.backgroundColor = 'green'
+          turn = true
+        }
+      } else if (!board[i].getAttribute("style") == null || board[i].getAttribute("style") == "") {
+        console.log('Square is already taken')
+      }
+    })
+  }
+}
+
+function winner(board, player) {
+  // row 1
+  if (board[0].classList.contains(player) && board[1].classList.contains(player) && board[2].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //row 2
+  else if (board[3].classList.contains(player) && board[4].classList.contains(player) && board[5].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //row 3
+  else if (board[6].classList.contains(player) && board[7].classList.contains(player) && board[8].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //column 1
+  else if (board[0].classList.contains(player) && board[3].classList.contains(player) && board[6].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //column 2
+  else if (board[1].classList.contains(player) && board[4].classList.contains(player) && board[7].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //column 3
+  else if (board[2].classList.contains(player) && board[5].classList.contains(player) && board[8].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  //diagonal right to left 
+  else if (board[0].classList.contains(player) && board[4].classList.contains(player) && board[8].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  }
+  // left to right
+  else if (board[2].classList.contains(player) && board[4].classList.contains(player) && board[6].classList.contains(player)) {
+    console.log(player + ' Wins')
+    return true
+  } else {
+    // draw
+    let counter = 0
     for (let i = 0; i < board.length; i++) {
-        board[i].addEventListener('click', (event) => {
-            if (turn) {
-                event.target.classList.add('player1')
-                event.target.style.backgroundColor = 'red'
-                turn = false
-            } else {
-                event.target.classList.add('player1')
-                event.target.style.backgroundColor = 'green'
-                turn = true
-            }
-        })
+      if (board[i].classList.contains(player)) {
+        if (counter === 9) {
+          console.log('Draw')
+          return null
+        } else if (!board[i].classList.contains(player)) {
+          return false
+        }
+        counter++
+      }
     }
+  }
 }
 game(board)
+
 
