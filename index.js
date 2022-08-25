8eaa2b520b1b373ace6517950115ac00b7163ffc
George <Georgesimbe@icloud.com>
2022-08-25 16:42:11 +0930

    Fix git files, fix draw

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let board = document.querySelectorAll('.box')
 let turn = true;
+let counter = 0;
 
 class players {
   constructor(id, name, playerStatus, color, wins, loss, draws) {
@@ -13,11 +14,13 @@ class players {
     this.draw = draws;
   }
 }
+
 let player1 = new players('X', 'John', 'player1', 'red', 0, 0, 0)
 let player2 = new players('O', 'Smith', 'player2', 'green', 0, 0, 0)
 
 // Rename Function name
 function game(board) {
+
   for (let i = 0; i < board.length; i++) {
     board[i].addEventListener('click', (event) => {
       if (!board[i].getAttribute('style') == null || board[i].getAttribute('style') == "") {
@@ -27,6 +30,8 @@ function game(board) {
         if (turn) {
           event.target.classList.add(player1.playerStatus)
           event.target.style.backgroundColor = player1.color
+          counter++
+          // console.log(counter)
           if (winner(board, turn ? player1.playerStatus : player2.playerStatus)) {
             endgame()
           }
@@ -34,20 +39,35 @@ function game(board) {
         } else {
           event.target.classList.add(player2.playerStatus)
           event.target.style.backgroundColor = player2.color
+          counter++
+          // console.log(counter)
           if (winner(board, turn ? player1.playerStatus : player2.playerStatus)) {
             endgame()
           }
           turn = true
         }
+
+        console.log(counter)
+        if (counter === 9) {
+          console.log("DRAWWWWW")
+          // if (winner(board, turn ? player1.playerStatus : player2.playerStatus))
+          //   console.log(turn ? player1.playerStatus : player2.playerStatus + " Wins")
+          // else {
+          //   console.log('draw')
+          // }
+        }
       }
     })
   }
-}
 
+  // console.log(counter + " Draw function")
+  // if (counter == 9) {
+  //   console.log(draw(board, turn ? player1.playerStatus : player2.playerStatus))
+  // }
+}
 function endgame() {
   console.log('Game end')
 }
-
 
 // // draw for game logic
 // let draw = false;
@@ -60,14 +80,8 @@ function endgame() {
 // }
 
 // function draw(board, player) {
-//   for (let i = 0; i < board.length; i++) {
-//     if (board[i].style.backgroundColor != player1.color && board[i].style.backgroundColor != player2.color) {
-//       return false
-//     }
-//   }
-//   return true
-// }
 
+// }
 
 function winner(board, player) {
   // row 1
@@ -113,5 +127,3 @@ function winner(board, player) {
 }
 
 game(board)
-
-
